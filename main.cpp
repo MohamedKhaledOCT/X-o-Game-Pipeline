3b83cfa2dd2432f87398ad6a0644a84d38f06284
Your Name <you@example.com>
2024-06-27 02:03:58 +0300

    2

--- main.cpp
+++ main.cpp
@@ -7,15 +7,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//
-    //
-    //
+
     QSqlDatabase tic=QSqlDatabase::addDatabase("QSQLITE");;
     tic.setDatabaseName("C:/Qt/users_.db");
     if(!tic.open())
     {
         qDebug()<<"problem";
     }
+    qDebug()<<"problem";
 
     MainWindow w;
     w.show();
